4ca5064031d2e17d8244f7ade31697cce0c86cf3
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2024-07-02 22:46:13 +0200

    Code refactoring

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,6 @@
 const config = {
   // Services
-  USERS_SERVICE: 'http://localhost:3000',
-  UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
+  UI_URL_PREFIX: '',
   ORDER_API_URL: 'http://localhost:8080/api/orders'
 };
 
